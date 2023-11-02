43cfdd8c19b040f7cac887aedb7ea0a1cc40eb7a
Samuel <samuel.cristhian@aluno.uece.br>
2023-11-02 16:44:23 -0300

    Subindo as dividas técnicas

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import output.Menu;
-
+//Dividas técnicas:
+//1. Não há um método para remover médico
+//2. Não existe tratamento de excceções nas funções de input
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
